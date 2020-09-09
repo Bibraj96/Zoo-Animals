246e64bad4771c7fdb3d6c355fd480baa7d7c45b
bibraj96 <blendi.ibraj96@gmail.com>
2020-09-09 15:38:04 -0400

    add seed data

--- zoo_animals_backend/db/seeds.rb
+++ zoo_animals_backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bronx = Zoo.create(name: "Bronx Zoo", city: "New York", state: "New York")
+central_park = Zoo.create(name: "Central Park Zoo", city: "New York", state: "New York")
+san_diego = Zoo.create(name: "San Diego Zoo", city: "San Diego", state: "California")
+
+tortoise = Sighting.create(animal: "Aldabra Tortoise", exhibit: "Zoo Center", schedule: "Open year-round", date: "04/14/20", accessibility: "Wheelchair Accessible", description: "I had no idea that tortoises could be so large!", zoo: bronx)
+panda = Sighting.create(animal: "Red Panda", exhibit: "Temperate Territory", schedule: "Open year-round", date: "06/25/20", accessibility: "Steep Hill", description: "This is the most adorable panda I've ever seen. Hard to see in the trees sometimes though", zoo: central_park)
+fox = Sighting.create(animal: "Arctic Fox", exhibit: "Northern Frontier", schedule: "Late fall - winter", date: "01/30/20", accessibility: "Wheelchair Accessible", description: "These foxes look so peaceful in the snow.", zoo: san_diego)
