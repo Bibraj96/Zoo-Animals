7f4ed5a9dd39f9aca66561e2a8daefff44bbdd86
bibraj96 <blendi.ibraj96@gmail.com>
2020-09-10 14:13:12 -0400

    add dom content loaded event listener to load all zoos

--- zoo_animals_frontend/src/zoo.js
+++ zoo_animals_frontend/src/zoo.js
@@ -1,16 +1,21 @@
-fetch("http://localhost:3000/zoos")
-.then((res) => res.json())
-.then((data) => {
-  let output = '<h2>Zoos</h2>'
-  data.forEach(function(zoo){
-    output += `
-      <ul>
-        <li>Name: ${zoo.name}</li>
-        <li>City: ${zoo.city}</li>
-        <li>State: ${zoo.state}</li>
-        <li>Sightings: ${zoo.sightings}</li>
-      </ul>
-    `;
-  });
-  document.getElementById('zoos').innerHTML = output;
-})
+const getZoos = () => {
+  fetch("http://localhost:3000/zoos")
+  .then((res) => res.json())
+  .then((data) => {
+    let zoos = document.getElementById('zoos');
+    let output = '<h2>Zoos</h2>'
+    data.forEach(function(zoo){
+      output += `
+        <ul>
+          <li>Name: ${zoo.name}</li>
+          <li>City: ${zoo.city}</li>
+          <li>State: ${zoo.state}</li>
+          <li>Sightings: ${zoo.sightings}</li>
+        </ul>
+      `;
+    });
+    zoos.innerHTML = output;
+  })
+}
+
+document.addEventListener("DOMContentLoaded", getZoos)
