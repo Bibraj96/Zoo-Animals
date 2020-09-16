9ad0490361ff94346543d1b39de1f3e3ea1fd249
bibraj96 <blendi.ibraj96@gmail.com>
2020-09-16 15:35:45 -0400

    add fetch request in loadSightings function

--- zoo_animals_frontend/src/sighting.js
+++ zoo_animals_frontend/src/sighting.js
@@ -1,4 +1,21 @@
 function loadSightings() {
-  console.log("This button works")
-  console.log(this.parentElement.getAttribute('data-zoo-id'))
+  let zooId = this.parentElement.getAttribute('data-zoo-id')
+  let sightingDiv = document.getElementById(`sighting-${zooId}`)
+
+  fetch("http://localhost:3000/sightings")
+  .then(resp => resp.json())
+  .then(data => {
+    data.forEach(function(sighting) {
+      if (sighting.zooId == zooId) {
+
+      }
+      // if(sighting.zoo.id == zooId) {
+      //   console.log(sighting.animal)
+      // } else {
+      //   console.log(sighting.id)
+      // }
+    })
+    })
+  // console.log("This button works")
+  // console.log(this.parentElement.getAttribute('data-zoo-id'))
 }
