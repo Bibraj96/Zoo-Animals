d7727dcda567220da1bde8384580c53599c3f775
bibraj96 <blendi.ibraj96@gmail.com>
2020-09-17 13:18:59 -0400

    add validations for presence of all sightings attributes

--- zoo_animals_backend/app/models/sighting.rb
+++ zoo_animals_backend/app/models/sighting.rb
@@ -1,3 +1,10 @@
 class Sighting < ApplicationRecord
   belongs_to :zoo
+
+  validates :animal, presence: true
+  validates :exhibit, presence: true
+  validates :schedule, presence: true
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :accessibility, presence: true
 end
