9b47a01d5fab275dd879a66221b4c41eeaf7f352
bibraj96 <blendi.ibraj96@gmail.com>
2020-09-17 15:30:41 -0400

    add create action

--- zoo_animals_backend/app/controllers/sightings_controller.rb
+++ zoo_animals_backend/app/controllers/sightings_controller.rb
@@ -3,4 +3,11 @@ class SightingsController < ApplicationController
     sightings = Sighting.all
     render json: sightings
   end
+
+  def create
+    sighting = Sighting.create(sighting_params)
+    render json: sighting
+  end
+
+  
 end
